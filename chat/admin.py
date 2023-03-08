5ebd4aa2b66375c8f186f955df41250c5311a46c
armansaheb1 <armansaheb@gmail.com>
2023-03-08 13:03:02 +0000

    Update

--- chat/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ChatSession , ChatSessionMessage
-# Register your models here.
-
-admin.site.register(ChatSession)
-admin.site.register(ChatSessionMessage)
