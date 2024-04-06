62cda32b90c387923d19c5f115f8fc30faca2121
armansaheb1 <armansaheb@gmail.com>
2024-04-06 07:43:39 +0000

    Update

--- sarafi/settings.py
+++ sarafi/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-ra$$l%4d2y4i!5#=(n58_u988+gdn8$st2^rtx-l0buiwhi7kc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['amizax.com', 'www.amizax.com', '80.71.149.175', '154.91.170.246', 'localhost', 'admin.amizax.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
